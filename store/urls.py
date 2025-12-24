86e706f805b5be794353e47b422437cbe4fc4459
TuretayevRauan <rturetaev23@apec.edu.kz>
2025-12-24 20:08:47 +0500

    inital commit

---
+++ store/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from storeProducts.views import index
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", index, name='index'),
+    path('products/',include('storeProducts.urls', namespace='product')),
+    path('users/',include('users.urls', namespace='users')),
+]
